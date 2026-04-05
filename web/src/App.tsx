d8d3154a4bd41cab7a76a25d16d99e65d83d3dfe
Victor Tesseroli <victortesseroli2@gmail.com>
2026-04-04 22:51:39 -0300

    feat: Tirando indexação da página de acompanhamento

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,27 +1,32 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async'; // Importe o Provider
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
 import { Adocao } from './pages/Adocao';
 import { Desaparecidos } from './pages/Desaparecidos';
 import { RegistrarOcorrencia } from './pages/RegistrarOcorrencia';
 import { Sobre } from './pages/Sobre';
+import Acompanhamento from './pages/Acompanhamento'; // Importe a nova página
 import { ScrollToTop } from './components/ScrollToTop';
 
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <div className="min-h-screen bg-gray-50">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/adocao" element={<Adocao />} />
-          <Route path="/desaparecidos" element={<Desaparecidos />} />
-          <Route path="/registrar-ocorrencia" element={<RegistrarOcorrencia />} />
-          <Route path="/sobre" element={<Sobre />} /> {/* Adicione esta linha */}
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <HelmetProvider> {/* Envolva toda a aplicação aqui */}
+      <BrowserRouter>
+        <ScrollToTop />
+        <div className="min-h-screen bg-gray-50">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/adocao" element={<Adocao />} />
+            <Route path="/desaparecidos" element={<Desaparecidos />} />
+            <Route path="/registrar-ocorrencia" element={<RegistrarOcorrencia />} />
+            <Route path="/acompanhar" element={<Acompanhamento />} />
+            <Route path="/sobre" element={<Sobre />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
